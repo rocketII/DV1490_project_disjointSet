97fde3871177add4f7b479a6588d239eab516fbc
rocketII <vangandr@gmail.com>
2016-05-06 13:55:25 +0200

    Initial commit

---
+++ DisjointSets.h
@@ -0,0 +1,23 @@
+#ifndef DISJOINT_SET_H
+#define DISJOINT_SET_H
+
+using namespace std;
+
+class DisjointSets
+{
+private:
+	int* set;
+	int size;
+public:
+	DisjointSets(int size);
+	DisjointSets(const DisjointSets &orig);
+	virtual ~DisjointSets();
+	DisjointSets& operator=(const DisjointSets &orig);
+	int find(int x) const;
+	int findCompress(int x);
+	void unionSets(int root1, int root2);
+	void unionSetsRank(int root1, int root2);
+	int maxHeight() const;
+};
+
+#endif
